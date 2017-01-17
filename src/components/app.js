cc22ca1ca5b073a9c1ce0e906e0ca92514e1f428
Ethan You <ethan_you@amaxchina.local>
2017-01-17 15:07:03 +0800

    header&left

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,46 @@
 import React, {Component} from 'react';
-// import {originalName as alias } from 'module';
-// import appcss from './app.css';
-// require("./app.css")
+// import {originalName as alias } from 'module'; import appcss from
+// './app.css'; require("./app.css")
 require("./app.styl")
 import yeoman from './yeoman.png';
 import myImg from '../images/1.jpg';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+         this.state = { data: [] };
+    };
+
+    componentDidMount() {
+        window
+        fetch('http://localhost:8000/articles/', {
+            method:"GET",
+            mode:"cros",
+           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        }
+            ).then(function (response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+            .then((data) => this.setState({data:data.results}))
+            .catch(error =>console.log(error));
+
+    };
+
     render() {
-        return (
-            <div className="test">
-                Hello React!222111222211!
-                12312<br />
-                <img src={yeoman} />
-                <img src={myImg} />
-            </div>
-        );
+        return ( 
+            <div className = "test" >  
+                <img src = {yeoman} />
+                <ul>
+                    {
+                        this.state.data.map(item =><li key={item.id}>{item.title}</li>)
+                    }
+                </ul>
+            </div>);
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app.js';
+import Header from './components/partitial/header';
+import LeftBar from './components/partitial/siderbar';
 
 ReactDom.render(
-    <App />,
+    <div>
+        <Header />
+        <LeftBar />
+        <br /><br /><br />
+        <App />
+                
+    </div>,
     document.getElementById("app")
 );
